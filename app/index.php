1bd37d1fd626586d99a66334c8f87acde10c3c86
Sampathkumar27896 <sampathkumar27896@gmail.com>
2018-08-15 10:37:02 +0530

    Worked on blog design

---
+++ app/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <script src="js/index.js"></script>
+</head>
+<body>
+    <p>Welcome Home</p>
+    
+
+    <div id="blog_card" style="background-image:url('assets/images/tulips.jpg')">
+        <!-- <img src="assets/images/night.jpg"  id="blog_picture" > -->
+        <div class="container">
+            <p id="blog_heading">Hello world</p>
+            <div id="blog_content">
+                <p>
+                    This is the simple content of the blog
+                    is the simple contentis the simple content
+                    is the simple content 
+                    is the simple content 
+                    is the simple content 
+                    is the simple content 
+                    is the simple content 
+                    is the simple content 
+                    is the simple content 
+
+                </p>
+            </div>
+            
+            <input type="button" value="ok" id="blog_ok">
+        </div>
+    </div>  
+        
+        
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
